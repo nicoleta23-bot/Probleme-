31d77e7f5a89c6d97726a71dc5ecc19e8bf1a9b9
nicoleta <71027700+nicoleta23-bot@users.noreply.github.com>
2021-01-13 21:59:07 +0300

    Add files via upload

---
+++ problema 5 f.py
@@ -0,0 +1,12 @@
+#	Afişaţi tabla înmulţirii cu numărul n.
+#  Exemplu: pentru n=5, se va afişa pe verticală 1x5=5  2x5=10 3x5=15 4x5=20 5x5=25 6x5=30 7x5=35 8x5=40 9x5=45 10x5=50. 
+# Din fişierul « numar.txt » se citeşte un număr, 
+# în fişierul « inmultire.txt » - se înscrie tabla înmulţirii cu acest număr.
+
+with open('numar', 'r') as f:
+    n=f.readline()
+for i in range(1,11):
+    x=str(i)+"*"+n+"="+str(i*int(n))
+    with open("inmultire.txt","a") as f:
+        f.write(x)
+        f.write("\n")
